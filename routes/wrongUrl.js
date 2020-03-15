952191a5bce8a4b05e353094732411b2bb117be4
Max Konovalov <maxkonovalov82@yandex.ru>
2020-03-15 17:01:29 +0300

    first revision id done

--- routes/wrongUrl.js
+++ routes/wrongUrl.js
@@ -1,6 +1,6 @@
 /* eslint-disable quotes */
 const wrongUrl = (req, res) => {
-  let regex = /^(users|cards).*/;
+  const regex = /^(users|cards).*/;
   if (!regex.test(req.params.value)) {
   // eslint-disable-next-line quote-props
     res.send({ "message": "Запрашиваемый ресурс не найден" });
