ebf816aa59a33dd181c4e23f574a8011a7de8ae3
Maxim Konovalov <maxkonovalov82@yandex.ru>
2021-01-10 11:46:16 +0300

    mquery latest version installed

--- app.js
+++ app.js
@@ -5,22 +5,40 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
-const PORT = require('./config');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const { PORT, SERVERADRESS, mongoConfig } = require('./constants/config');
 
 const users = require('./controllers/users');
 const auth = require('./middlewares/auth');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-})
+mongoose.connect(`mongodb://${SERVERADRESS}:27017/mestodb`, mongoConfig)
   .then(() => console.log('mongoose is running'))
   .catch((err) => console.log(err.message));
 
+// CORS middleware applying
+const whitelist = [
+  'http://localhost:8080',
+  'http://localhost:3000',
+  // 'http://news-page.gq',
+  // 'https://news-page.gq',
+  // 'https://konovaly4.github.io/news-page-frontend.github.io',
+  // 'https://konovaly4.github.io',
+];
+const corsOptions = {
+  credentials: true,
+  origin: whitelist,
+};
+app.options('*', cors(corsOptions), (req, res) => {
+  res.status(200).send('OK');
+});
+app.use(cors(corsOptions));
+
+// other app use
 app.use(helmet());
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
---
+++ constants/config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const {
+  PORT = 3000, NODE_ENV, JWT_SECRET, SERVERADRESS,
+} = process.env;
+
+const mongoConfig = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
+
+module.exports = { PORT, mongoConfig };
+module.exports.JWT_SECRET = NODE_ENV === 'production' ? JWT_SECRET : 'test-key';
+module.exports.SERVERADRESS = NODE_ENV === 'production' ? SERVERADRESS : 'localhost';
